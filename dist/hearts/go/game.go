43409dec5aa55e35d1987a8afde85cd262546d0a
phillc <spyyderz@gmail.com>
2013-09-09 19:05:53 -0400

    To a point where two of clubs is played for the go agent

--- dist/hearts/go/game.go
+++ dist/hearts/go/game.go
@@ -5,13 +5,165 @@ import (
 	"syscall"
 	"git.apache.org/thrift.git/lib/go/thrift"
 	"./lib/AgentVsAgent"
+	"errors"
 )
 
-func runGame() {
+/*func (ex *AgentVsAgent.GameException) Error() string {*/
+/*	return "errr????"*/
+/*}*/
+
+type passCardFn func(*Round) []*AgentVsAgent.Card
+type playCardFn func(*Trick) *AgentVsAgent.Card
+
+type options struct {
+	ticket *AgentVsAgent.Ticket
+	client *AgentVsAgent.HeartsClient
+	doPassCards *passCardFn
+	doPlayCard *playCardFn
 }
 
-type passCardFn func() string
-type playCardFn func() string
+type Trick struct {
+	number int
+	round *Round
+	leader string
+	played []*AgentVsAgent.Card
+}
+
+func (trick *Trick) run(opts *options) (err error) {
+	trick.log("Starting trick")
+	currentTrick, ex, err := opts.client.GetTrick(opts.ticket)
+	if err != nil { return err }
+	if ex != nil { return errors.New((*ex).String()) }
+	trick.leader = string(currentTrick.Leader)
+	trick.played = currentTrick.Played
+
+	cardToPlay := (*opts.doPlayCard)(trick)
+
+	trickResult, ex, err := opts.client.PlayCard(opts.ticket, cardToPlay)
+	if err != nil { return err }
+	if ex != nil { return errors.New((*ex).String()) }
+
+	trick.log("trick: result", trickResult)
+	trick.played = trickResult.Played
+	return
+}
+
+func (trick Trick) log(message ...interface{}) {
+	newMessage := append([]interface{}{"T:", trick.number}, message...)
+	trick.round.log(newMessage...)
+}
+
+type Round struct {
+	number int
+	tricks []*Trick
+	dealt []*AgentVsAgent.Card
+	held []*AgentVsAgent.Card
+	game *Game
+}
+
+func (round *Round) createTrick() Trick {
+	trick := Trick{number: len(round.tricks) + 1, round: round}
+	round.tricks = append(round.tricks, &trick)
+	return trick
+}
+
+func (round *Round) run(opts *options) (err error) {
+	hand, ex, err := opts.client.GetHand(opts.ticket)
+	if err != nil { return err }
+	if ex != nil { return errors.New((*ex).String()) }
+	round.log("You were dealt:", hand)
+	round.dealt = hand
+	round.held = hand
+
+	err = round.passCards(opts)
+	if err != nil { return err }
+	return round.playTrick(opts)
+}
+
+func (round *Round) passCards(opts *options) (err error) {
+	if round.number % 4 == 0 {
+		round.log("Not passing cards")
+	} else {
+		round.log("About to pass cards")
+		cardsToPass := (*opts.doPassCards)(round)
+
+		var newHeld []*AgentVsAgent.Card
+		for i := 0; i < len(round.held); i++ {
+			toRemove := false
+			card := round.held[i]
+
+			for j := 0; j < len(cardsToPass); j++ {
+				if cardsToPass[j] == card {
+					toRemove = true
+				}
+			}
+
+			if !toRemove {
+				newHeld = append(newHeld, card)
+			}
+		}
+
+		receivedCards, ex, err := opts.client.PassCards(opts.ticket, cardsToPass)
+		if err != nil { return err }
+		if ex != nil { return errors.New((*ex).String()) }
+		round.log("Received cards:", receivedCards)
+		round.held = append(newHeld, receivedCards...)
+	}
+	return err
+}
+
+func (round *Round) playTrick(opts *options) (err error) {
+	trick := round.createTrick()
+	err = trick.run(opts)
+	if err != nil { return err }
+
+	if len(round.tricks) < 13 {
+		err = round.playTrick(opts)
+	}
+	return err
+}
+
+func (round Round) log(message ...interface{}) {
+	newMessage := append([]interface{}{"R:", round.number}, message...)
+	round.game.log(newMessage...)
+}
+
+type Game struct {
+	rounds []Round
+	info *AgentVsAgent.GameInfo
+}
+
+func (game *Game) createRound() Round {
+	round := Round{number: len(game.rounds) + 1, game: game}
+	game.rounds = append(game.rounds, round)
+	return round
+}
+
+func (game Game) run(opts *options) (err error) {
+	game.log("Starting game")
+
+	round := game.createRound()
+
+	err = round.run(opts)
+	if err != nil { return err }
+
+	roundResult, ex, err := opts.client.GetRoundResult(opts.ticket)
+	if err != nil { return err }
+	if ex != nil { return errors.New((*ex).String()) }
+
+	game.log("round result:", roundResult)
+	if roundResult.Status == AgentVsAgent.GameStatus_NEXT_ROUND {
+		err = game.run(opts)
+	}
+	// get round result
+	//if status not next round
+	return err
+}
+
+func (game Game) log(message ...interface{}) {
+	newMessage := append([]interface{}{"P:", game.info.Position}, message...)
+	fmt.Println(newMessage...)
+}
 
 func play(doPassCards passCardFn, doPlayCard playCardFn) {
 	host, hostFound := syscall.Getenv("AVA_HOST")
@@ -51,14 +203,18 @@ func play(doPassCards passCardFn, doPlayCard playCardFn) {
 	ticket := response.Ticket
 	if ticket != nil {
 		fmt.Println("playing")
-		game_info, _, _ := client.GetGameInfo(ticket)
-		fmt.Println("game info:", game_info)
+		gameInfo, _, _ := client.GetGameInfo(ticket)
+		fmt.Println("game info:", gameInfo)
 
-		runGame()
-		// runGame(ticket, client, doPassCards, doPlayCard)
+		game := Game{info: gameInfo}
+		err = game.run(&options{ticket: ticket, client: client, doPassCards: &doPassCards, doPlayCard: &doPlayCard})
+		if err != nil {
+			fmt.Println("ERROR:", err)
+			return
+		}
 		fmt.Println("Game is over")
-		game_result, _, _, _ := client.GetGameResult(ticket)
-		fmt.Println("game result:", game_result)
+		gameResult, _, _ := client.GetGameResult(ticket)
+		fmt.Println("game result:", gameResult)
 	} else {
 		fmt.Println("No ticket")
 		return
--- dist/hearts/go/my_agent.go
+++ dist/hearts/go/my_agent.go
@@ -1,13 +1,51 @@
 package main
 
-func passCards() string {
-	return "hello"
+import (
+	"./lib/AgentVsAgent"
+)
+
+func isLeadingTrick(trick *Trick) bool {
+	return len(trick.played) == 0
 }
 
-func playCard() string {
-	return "hey"
+func isHeartsBroken(trick *Trick) bool {
+	return true
+}
+
+func onlyTwoClubs(cards []*AgentVsAgent.Card) []*AgentVsAgent.Card {
+	var matchedCards []*AgentVsAgent.Card
+	for i := 0; i < len(cards); i++ {
+		if cards[i].Suit == AgentVsAgent.Suit_CLUBS && cards[i].Rank == AgentVsAgent.Rank_TWO {
+			matchedCards = append(matchedCards, cards[i])
+		}
+	}
+	return matchedCards
+}
+
+func playableCards(trick *Trick) []*AgentVsAgent.Card {
+	validCards := trick.round.held
+
+	if trick.number == 1 && isLeadingTrick(trick) {
+		validCards = onlyTwoClubs(validCards)
+	}
+	trick.log("Valid cards:", validCards)
+	return validCards
+}
+
+func doPassCards(round *Round) []*AgentVsAgent.Card {
+	cardsToPass := round.dealt[0:3]
+	round.log("Passing cards", cardsToPass)
+
+	return cardsToPass
+}
+
+func doPlayCard(trick *Trick) *AgentVsAgent.Card {
+	trick.log("Current trick:", trick)
+	cardToPlay := playableCards(trick)[0]
+	trick.log("Playing card:", cardToPlay)
+	return cardToPlay
 }
 
 func main() {
-	play(passCards, playCard)
+	play(doPassCards, doPlayCard)
 }
