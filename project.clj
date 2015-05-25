0c64a82ef0b29eb1d4387da26a83cb90f83fc098
phillc <spyyderz@gmail.com>
2015-05-25 11:53:38 -0400

    Well, I know it works now

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject agent-vs-agent "0.1.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/clojurescript "0.0-2173"]
+                 [org.clojure/clojurescript "0.0-2760"]
                  [org.omcljs/om "0.8.8"]]
-  :plugins [[lein-cljsbuild "1.0.2"]]
+  :plugins [[lein-cljsbuild "1.0.6"]]
   :hooks [leiningen.cljsbuild]
   :source-paths ["src/clj"]
   :cljsbuild {
--- web/app/core.cljs
+++ web/app/core.cljs
@@ -1,8 +1,18 @@
 (ns ava.core
   (:require [om.core :as om]
-            [om.dom :as dom]))
-
+            [om.dom :as dom])
+)
 (enable-console-print!)
 
+(println "HI")
+(println (+ 5 3 3))
 
-(println (+ 3 2))
+(defonce app-state (atom {:list ["Lion" "Zebra" "Buffalo" "Antelope"]}))
+
+(om/root
+ (fn [data owner]
+   (om/component
+    (apply dom/ul #js {:className "animals"}
+           (map (fn [text] (dom/li nil text)) (:list data)))))
+ app-state
+ {:target (. js/document (getElementById "hearts-app"))})
