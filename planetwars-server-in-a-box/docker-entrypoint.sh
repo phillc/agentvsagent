406cb299253bf78b68dfe98136a7b81391c9cb29
phillc <15082+phillc@users.noreply.github.com>
2020-03-27 10:40:53 -0400

    Server failed until first rating

--- planetwars-server-in-a-box/docker-entrypoint.sh
+++ planetwars-server-in-a-box/docker-entrypoint.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 set -e
 
+rm /home/data/rate.lock
 touch /home/data/users.txt
 touch /home/data/ratings.txt
 touch /home/data/results.pgn
 
 mkdir -p /var/www/tmp
 touch /var/www/results.pgn
+touch /var/www/ratings.txt
 
 exec "$@"
